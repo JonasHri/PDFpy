25678cadd60d5048ada868b7c8a641825d9ab9fd
JonasHri <jonasharriehausen@gmail.com>
2022-01-11 22:03:30 +0100

    changed internal Page handling to Python List

--- PDFpy/PDFpy_main.py
+++ PDFpy/PDFpy_main.py
@@ -1,87 +1,44 @@
 from PyPDF2 import PdfFileWriter, PdfFileReader
-from collections.abc import Iterable
 
 class PDF:
     '''A Simple Implementation of a PDF editor based on PyPDF2'''
     def __init__(self, path = None):
-        self.data = PdfFileWriter()
+        self.data = []
         if path != None:
             file = PdfFileReader(open(path, "rb"), strict=False)
             for i in range(file.getNumPages()):
-                self.data.addPage(file.getPage(i))
+                self.data.append(file.getPage(i))
 
     def __len__(self):
-        return self.data.getNumPages()
+        return len(self.data)
     
     def __add__(self, other):
         assert isinstance(other, PDF)
 
         res = PDF()
-        for i in range(len(self)):
-            res.data.addPage(self.data.getPage(i))
-        
-        for i in range(len(other)):
-            res.data.addPage(other.data.getPage(i))
+
+        res.data = self.data + other.data
 
         return res
 
     def __getitem__(self, key):
         res = PDF()
 
-        if isinstance(key, int):
-            res.data.addPage(self.data.getPage(key))
-
-        elif isinstance(key, Iterable):
-            for i in key:
-                res.data.addPage(self.data.getPage(i))
-        
-        elif isinstance(key, slice):
-            key = key.indices(len(self))
-            for i in range(key[0], key[1], key[2]):
-                res.data.addPage(self.data.getPage(i))
+        res.data = self.data[key]
     
         return res
 
     def __setitem__(self, key, other):
-        assert isinstance(other, PDF)
-
-        if isinstance(key, int):
-            self.__setitem__([key], other)
-            return
-        elif isinstance(key, slice):
-            key = key.indices(len(self))
-            self.__setitem__(list(range(key[0], key[1], key[2])), other)
-            return
-        
-        assert len(key) == len(other)
-
-        res = PdfFileWriter()
-        for i in range(len(self)):
-            if i in key:
-                idx = key.index(i)
-                res.addPage(other.data.getPage(idx))
-            else:
-                res.addPage(self.data.getPage(i))
-        
-        self.data = res
+        self.data[key] = other.data
 
     def __delitem__(self, key):
-        if isinstance(key, int):
-            self.__delitem__([key])
-            return
-        elif isinstance(key, slice):
-            key = key.indices(len(self))
-            self.__delitem__(list(range(key[0], key[1], key[2])))
-            return
-        res = PdfFileWriter()
-        for i in range(len(self)):
-            if i not in key:
-                res.addPage(self.data.getPage(i))
-
-        self.data = res
+        del self.data[key]
         
             
     def save(self, path):
         '''Saves the PDF at the designated path or the original Location if no path is given'''
+        writer = PdfFileWriter()
+        for page in self.data:
+            writer.addPage(page)
         with open(path, "wb") as f:
-            self.data.write(f)
+            writer.write(f)
